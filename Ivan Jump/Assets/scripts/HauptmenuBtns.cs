a5e8b37fcee49432469fb97513f22a8a956e1614
2L0sT4u <dennis.duergerow@gmail.com>
2022-09-15 21:45:32 +0200

    Ivan-Jump v0.1.2
    Erstellen des Level Menüs
    Hinzufügen der Funktionen der Buttons

---
+++ Ivan Jump/Assets/scripts/HauptmenuBtns.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HauptmenuBtns : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Spielen_btn_pressed()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    public void Ende_btn_pressed()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
